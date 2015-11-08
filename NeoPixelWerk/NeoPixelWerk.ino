ac9bba201816bf07d68d2d9483ed9925949e71be
Mark Ross <rossma@berea.edu>
2015-11-08 18:06:41 -0500

    added work into the NeoPixel demo

---
+++ NeoPixelWerk/NeoPixelWerk.ino
@@ -0,0 +1,30 @@
+#include "Adafruit_NeoPixel.h"
+
+const int LEDs = 7;
+const int PIN = 2;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDs, PIN, NEO_GRB + NEO_KHZ800);
+
+uint32_t red = 0xFF0000;
+    
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  strip.setPixelColor(0, 255, 0, 255);
+  strip.setPixelColor(1, 255, 255, 255);
+  strip.setPixelColor(2, 0, 255, 255);
+  strip.setPixelColor(3, 0, 0, 255);
+
+}
